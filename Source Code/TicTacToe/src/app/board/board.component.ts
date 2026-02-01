6c911a3c9a75cccdb7586a86d5d24b66d3c07724
Amey Thakur <ameythakur20@gmail.com>
2026-02-01 00:26:05 -0500

    Angular
    Co-authored-by: Mega Satish <mega.modha@gmail.com>

--- Source Code/TicTacToe/src/app/board/board.component.ts
+++ Source Code/TicTacToe/src/app/board/board.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
-    selector: 'app-board',
-    templateUrl: './board.component.html',
-    styleUrls: ['./board.component.css'],
-    imports: []
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css'],
+  imports: []
 })
 export class BoardComponent implements OnInit {
-  @Input() value: 'X' | 'O' | undefined
+  @Input() value: 'X' | 'O' | undefined;
+  @Input() isWinning: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
